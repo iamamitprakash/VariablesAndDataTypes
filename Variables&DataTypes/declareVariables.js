b256a58371dcb108997afbb53995671ab53fa731
Amit Prakash <prakashamit37@gmail.com>
2025-04-22 17:13:08 +0530

    Declare the varibles and try to reassign it

---
+++ Variables&DataTypes/declareVariables.js
@@ -0,0 +1,21 @@
+//Declare a variable in JS by using var, let and const
+
+var name = "Amit";
+let age = 42;
+const course_completed = "Full Stack Developer";//value will not be changed after assigning with const keyword, i.e- line no-16
+
+//Print all of them
+console.log(name);
+console.log(age);
+console.log(course_completed);
+
+//Try to reassigning it
+
+name = "Prakash";
+age = 40;
+// course_completed = "Frontend Developer";
+
+//Print all of them again
+console.log("After reassign the value:",name);
+console.log("After reassign the value:",age);
+// console.log(course_completed);
