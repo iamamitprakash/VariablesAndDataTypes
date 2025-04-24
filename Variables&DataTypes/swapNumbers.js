929471334872bb5d512e5b32156ca3555b7a1b88
Amit Prakash <prakashamit37@gmail.com>
2025-04-24 17:05:40 +0530

    Swap two numbers using third variable

---
+++ Variables&DataTypes/swapNumbers.js
@@ -0,0 +1,15 @@
+//Swap two numbers using third variable.
+
+let a = 32;
+let b = 45;
+let c;
+
+console.log("Before swap:");
+console.log("a =", a, "b =", b);
+
+c = a;
+a = b;
+b = c;
+
+console.log("After swap:");
+console.log("a =", a, "b =", b);
