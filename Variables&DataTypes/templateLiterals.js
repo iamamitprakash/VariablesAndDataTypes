c88d4b1c9d6458edbd9110dfe640e30a7f269a3f
Amit Prakash <prakashamit37@gmail.com>
2025-04-24 16:37:10 +0530

    Template literals completed

---
+++ Variables&DataTypes/templateLiterals.js
@@ -0,0 +1,8 @@
+//Create a sentence using template literls
+
+let name = "Amit Prakash";
+let age = 42;
+let course_completed = "FullStack Developer course";
+let designation = "SDE1";
+
+console.log(`My name is ${name} and I am ${age} years old. I have just completed ${course_completed} and looking for ${designation} role.`);
